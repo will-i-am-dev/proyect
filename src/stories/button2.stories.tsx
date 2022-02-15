7ba50883c283cceca83c0307fa3c803c2462ff16
will-i-am-dev <m24.1@hotmail.com>
2022-02-15 12:47:38 -0500

    fix config storybook  and add story button2

---
+++ src/stories/button2.stories.tsx
@@ -0,0 +1,30 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import Button from '../components/buttons/button2'
+
+export default {
+  title: 'Components/Buttons/Button',
+  component: Button,
+  argTypes: {
+    children: {
+      control: { type: 'text' }
+    }
+  }
+} as ComponentMeta<typeof Button>
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  children: 'Button',
+  variant: 'primary'
+}
+
+export const WithIcon = Template.bind({})
+WithIcon.args = {
+  children: (
+    <div className='flex gap-2 items-center'>
+
+      <span>Search</span>
+    </div>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        primary: {
+
+          50: '#d8fbff',
+          100: '#abeaff',
+          200: '#7bdbff',
+          300: '#48cbff',
+          400: '#1abcff',
+          500: '#00a2e6',
+          600: '#007eb4',
+          700: '#005a82',
+          800: '#003751',
+          900: '#001421'
+
+        },
+        dark: '#222222',
         darkBlue: '#00253d',
         white: '#fff',
         blue: '#00a1e4',
@@ -14,7 +29,6 @@ module.exports = {
         grayDark: '#273444',
         gray: '#f1f5f9',
         grayLight: '#c4c4c4',
-        primary: '#5c6ac4',
         success: '#13ce66'
       }
     }
