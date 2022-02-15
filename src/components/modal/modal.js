8f82b18a2fe5f04bcd7a78bb9b1b07f5afa1dafa
Jorge Eduardo <corpjorge@hotmail.com>
2022-02-15 13:08:23 -0500

    Update modal.js

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -32,7 +32,7 @@ export const Modal: React.FC<> = (props) => {
 
           <div className="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
             <button data-modal-toggle="defaultModal" type="button"
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I
+                    className="text-white bg-blue hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I
               accept
             </button>
             <button data-modal-toggle="defaultModal" type="button"
